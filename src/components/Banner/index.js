0868dfc485bc4b9dc58d2bb36a1f4efae7a2c824
krishnakumars59809 <krishnakumars59809work@gmail.com>
2023-02-01 13:29:42 +0530

    banner update

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,12 +1,14 @@
-const Banner = ({ bannerText, strapline, background = {}, link = {} }) => {
+const Banner = ({ bannerText, image, background = {}, link = {} }) => {
   console.log(bannerText);
   return (
-    <section className="banner">
+    <section className="banner px-28">
       <header>
-        <h1>{bannerText?.header}</h1>
+        <h1 className="text-red-700">{bannerText?.header}</h1>
         <h>{bannerText?.description}</h>
       </header>
-      <img src={background.image?.url().build()} alt={background.alt} />
+      <div className="grid" >
+     <div> <img src={image?.img?.image?.image?.url().build()} alt={background.alt} className="aspect-[2]"/></div>
+      </div>
       <a href={link.url}>{link.title}</a>
     </section>
   );
